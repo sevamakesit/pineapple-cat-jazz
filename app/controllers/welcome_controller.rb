0243123ee9bdc7c32db534cca8bc0ef4253c7392
Seva Rybakov <jumpincode@gmail.com>
2017-03-03 18:40:44 +0300

    manage courses

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -12,8 +12,4 @@ class WelcomeController < ApplicationController
   def pricing
   end
 
-  def courses
-    @courses = ['Course 1', 'Course 2', 'Course 3']
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
   get '/about' => "welcome#about"
   get '/faq' => "welcome#faq"
   get '/pricing' => "welcome#pricing"
-  get '/courses' => "welcome#courses"
+
+  resources :courses
+
+  # get '/courses' => 'courses#index'
+  # get '/courses/:id' => 'courses#show'
+  #
+  # get '/courses/:id/edit' => 'courses#edit'
+  # put '/courses/:id' => 'courses#update'
+  #
+  # get '/courses/new' => 'courses#new'
+  # post '/courses' => 'courses#create'
+  #
+  # delete '/courses/:id' => 'courses#destroy'
 end
